2338b4f3866eeb76537847e5c6c99056a981d69e
geonhoyun <geonhoyun@gmail.com>
2024-07-28 13:17:45 +0200

    Remove authorized callback

--- auth.config.ts
+++ auth.config.ts
@@ -1,16 +1,7 @@
-import NextAuth, { NextAuthConfig } from "next-auth";
-import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
-
+import { NextAuthConfig } from "next-auth";
 export const authConfig = {
     pages: {
         signIn: "/login",
     },
-    callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
-            return !!auth?.user;
-        },
-    },
-
     providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;

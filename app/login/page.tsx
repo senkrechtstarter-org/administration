68902c4cfdd9c8caa290fd4bc4a870ca29c045f8
geonhoyun <geonhoyun@gmail.com>
2024-08-28 02:08:01 +0200

    Add black background for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from "@/app/components/login-form";
 import { Card, CardHeader, CardFooter, Divider } from "@nextui-org/react";
 export default function LoginPage() {
     return (
-        <div className="flex items-center max-w-140 m-auto mt-auto justify-center h-[100vh]">
+        <div className="flex items-center max-w-140 m-auto mt-auto justify-center h-[100vh] bg-black">
             <Card className="bg-gradient-to-l from-cyan-400 to-blue-500">
                 <CardHeader className="h-20 items-end rounded-lg p-5">
                     <SenkrechtstarterLogo />
